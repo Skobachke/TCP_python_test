853fe29d4fed2591b7c883b3b5e8ba7840663510
Skobachke <skobachke@gmail.com>
2017-07-14 21:41:05 +0300

    Works server and cliend

--- tcpServer.py
+++ tcpServer.py
@@ -9,13 +9,13 @@ def Main():
     c, addr = s.accept()
     print("Connection from: " + str(addr))
     while True:
-        data = c.recv(1024)
+        data = c.recv(1024).decode()
         if not data:
             break
         print("from connected user: " + str(data))
         data = str(data).upper()
         print("sending: " + str(data))
-        c.send(data)
+        c.send(data.encode())
     c.close()
 
 if __name__ == '__main__':
